7d56becb4879d504922e10322a866c7f7933e6d7
nisa-b <nisa.input@gmail.com>
2025-12-14 22:43:59 +0300

    Update run_neuro.py

--- run_neuro.py
+++ run_neuro.py
@@ -2,21 +2,15 @@ import matplotlib.pyplot as plt
 from spike_encoding import wavelet_to_spikes
 from motor_neuron import motor_neuron
 
-# ---------------------------------------------------------
 # EVET and HAYIR spike generation
-# ---------------------------------------------------------
 evet_spikes = wavelet_to_spikes("data/evet/evet_01.wav")
 hayir_spikes = wavelet_to_spikes("data/hayir/hayir_01.wav")
 
-# ---------------------------------------------------------
 # Motor neuron processing
-# ---------------------------------------------------------
 evet_motor = motor_neuron(evet_spikes)
 hayir_motor = motor_neuron(hayir_spikes)
 
-# ---------------------------------------------------------
 # Visualization
-# ---------------------------------------------------------
 plt.figure(figsize=(10,4))
 
 plt.subplot(2,1,1)
@@ -32,3 +26,4 @@ plt.xlabel("Time step")
 
 plt.tight_layout()
 plt.show()
+
